e34b33b1ecbfe952ddc4bb3b88724a1515791c4f
Marcos <keickd16@gmail.com>
2021-04-05 17:45:49 +0200

    setting firebase 2

--- app/src/main/java/es/usj/androidapps/alu100485/ho/MainActivity.kt
+++ app/src/main/java/es/usj/androidapps/alu100485/ho/MainActivity.kt
@@ -1,40 +1,45 @@
 package es.usj.androidapps.alu100485.ho
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     var userOk = false
     var userPass = false
+    private lateinit var mFirebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mFirebaseAnalytics = Firebase.analytics
+        mFirebaseAnalytics.setUserProperty("user_type", "standard")
+
         etUser.addTextChangedListener(CustomTextWatcher({
-            if(!etUser.text.isNullOrBlank()) userOk = true
+            if (!etUser.text.isNullOrBlank()) userOk = true
             else userOk = false
 
             checkUserAndPass()
         }))
 
         etPassword.addTextChangedListener(CustomTextWatcher({
-            if(!etPassword.text.isNullOrBlank()) userPass = true
+            if (!etPassword.text.isNullOrBlank()) userPass = true
             else userPass = false
 
             checkUserAndPass()
         }))
 
         btnLogin.setOnClickListener {
+            mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, null)
             val intentFromAToB = Intent(this, Activity_b::class.java)
             startActivity(intentFromAToB)
         }
@@ -49,6 +54,11 @@ class MainActivity : AppCompatActivity() {
             btnLogin.isClickable = false
         }
     }
+
+    /*override fun onDestroy(){
+        super.onDestroy()
+        mFirebaseAnalytics.resetAnalyticsData()
+    }*/
 }
 
 class CustomTextWatcher(private val callback: () -> Unit) : TextWatcher {
